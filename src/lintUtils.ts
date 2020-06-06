e95f86be9999a96b7a01b72cce17b5fc0ae0108f
Tianxi Ku <kutianxi@outlook.com>
2020-06-06 16:24:43 +0800

    remove entire line tslint comment, including line endings

--- src/lintUtils.ts
+++ src/lintUtils.ts
@@ -1,14 +1,25 @@
 import { Replacement } from "tslint";
 import { SourceFile } from "typescript";
 
+function isEntireLineComment(line: string) {
+    return !!line.match(/^\s*\t*\/\/|^\s*\t*\/\*[^*]+\*\/\s*$/);
+}
+
 export function getTslintLineReplacement(
     line: string,
-    lineStartPosition: number
+    lineStartPosition: number,
+    isCRLFEnding: boolean
 ): Replacement | null {
     const tslintCommentMatchResult = line.match(
         /\s*\t*\/[*\/]\s*tslint:disable-(next-)?line[\w\s]*(\*\/)?\s*/
     );
     if (tslintCommentMatchResult !== null) {
+        if (isEntireLineComment(line)) {
+            return Replacement.deleteFromTo(
+                lineStartPosition,
+                line.length + lineStartPosition + (isCRLFEnding ? 2 : 1)
+            );
+        }
         const positionOfClosingComment = line.indexOf("*/");
         if (positionOfClosingComment !== -1) {
             return Replacement.deleteFromTo(
@@ -24,18 +35,26 @@ export function getTslintLineReplacement(
     return null;
 }
 
+function getIsCRLFEnding(file: SourceFile, lineEndOfPosition: number) {
+    return (
+        file.text.charAt(lineEndOfPosition) === "\r" &&
+        file.text.charAt(lineEndOfPosition + 1) === "\n"
+    );
+}
+
 export function removeTslintComments(file: SourceFile) {
     const replacements: Replacement[] = file
         .getLineStarts()
-        .map(lineStartPosition =>
-            getTslintLineReplacement(
-                file.text.substring(
-                    lineStartPosition,
-                    file.getLineEndOfPosition(lineStartPosition)
-                ),
+        .map(lineStartPosition => {
+            const lineEndOfPosition = file.getLineEndOfPosition(
                 lineStartPosition
-            )
-        )
+            );
+            return getTslintLineReplacement(
+                file.text.substring(lineStartPosition, lineEndOfPosition),
+                lineStartPosition,
+                getIsCRLFEnding(file, lineEndOfPosition)
+            );
+        })
         .filter(r => r !== null) as Replacement[];
     return Replacement.applyAll(file.text, replacements);
 }
--- test/lintUtils.test.ts
+++ test/lintUtils.test.ts
@@ -3,31 +3,45 @@ import { Replacement } from "tslint";
 
 describe("Remove tslint disable line comments", () => {
     it("should return empty replacement when line content does not contain tslint comment", () => {
-        expect(getTslintLineReplacement("\tsome normal code", 0)).toBeNull();
+        expect(
+            getTslintLineReplacement("\tsome normal code", 0, false)
+        ).toBeNull();
     });
     it("should return whole line replacement when line content only contains comment", () => {
         const lineContent = "    // tslint:disable-line";
-        expect(getTslintLineReplacement(lineContent, 0)).toEqual(
-            Replacement.deleteText(0, lineContent.length)
+        expect(getTslintLineReplacement(lineContent, 0, false)).toEqual(
+            Replacement.deleteText(0, lineContent.length + 1) // + 1 to remove line ending
         );
     });
     it("should return latter part replacement when comment is at the end of line", () => {
         const lineContent = "    const a = 100; // tslint:disable-line";
-        expect(getTslintLineReplacement(lineContent, 0)).toEqual(
+        expect(getTslintLineReplacement(lineContent, 0, false)).toEqual(
             Replacement.deleteText(18, 23)
+        );
+    });
+    it("should remove 2 character ending if isCRLF ending and the entire line is comment", () => {
+        const lineContent = "    // tslint:disable-next-line";
+        expect(getTslintLineReplacement(lineContent, 0, true)).toEqual(
+            Replacement.deleteText(0, 33)
         );
     });
     it("should return whole line replacement when line content is like /* tslint:disable */", () => {
         const lineContent = "    /* tslint:disable-next-line */";
-        expect(getTslintLineReplacement(lineContent, 100)).toEqual(
-            Replacement.deleteText(100, lineContent.length)
+        expect(getTslintLineReplacement(lineContent, 100, false)).toEqual(
+            Replacement.deleteText(100, lineContent.length + 1)
         );
     });
     it("should return mid partial replacement when /* tslint:disable */ is in the middle of a line", () => {
         const lineContent =
             "    const a = 100; /* tslint:disable-next-line */  const b = 200;";
-        expect(getTslintLineReplacement(lineContent, 100)).toEqual(
+        expect(getTslintLineReplacement(lineContent, 100, false)).toEqual(
             Replacement.deleteText(118, 31)
+        );
+    });
+    it("should remove entire line comment like /* tslint:disable-line no-any */", () => {
+        const lineContent = "    /* tslint:disable-next-line */  ";
+        expect(getTslintLineReplacement(lineContent, 100, false)).toEqual(
+            Replacement.deleteText(100, lineContent.length + 1)
         );
     });
 });
